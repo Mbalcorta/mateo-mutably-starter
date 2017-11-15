8e7418b548c483764473ba0d4c5cb6960de6cdbf
Mateo Balcorta <misterrattail@gmail.com>
2017-11-15 13:39:37 -0800

    Add files  and for express.

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require('express')
 const app = express()
+const ejs = require('ejs')
+const path = require('path')
 
-app.use(express.static('public'))
+
+app.use(express.static(path.join(__dirname, '/src/public')))
 
 // set 'html' as the engine, using ejs's renderFile function
-var ejs = require('ejs');
+
 app.engine('html', ejs.renderFile);
+app.set('views', __dirname + '/src/views')
 app.set('view engine', 'html');
 
 app.get('/', (request, response) => {
