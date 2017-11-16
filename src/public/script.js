89d7a11c0daee5b52c962bdad71e43e1e7497739
Mateo Balcorta <misterrattail@gmail.com>
2017-11-15 21:41:54 -0800

    Add edit button to toggle to save button when clicked.

--- src/public/script.js
+++ src/public/script.js
@@ -2,28 +2,78 @@ console.log("Sanity Check: JS is working!");
 
 $(document).ready(function(){
 
-  // code in here
-  $('#getAllBooks').on('click', function getAllBooksJson(e){
-    $("#all-books").toggle()
+  const getAllBooks = () => {
     fetch('https://mutably.herokuapp.com/books').then((response) => {
       const contentType = response.headers.get('content-type');
       if(contentType && contentType.includes('application/json')) {
         return response.json()
       }
       throw new TypeError('oops, we haven\'t got JSON!')
-    }).then((bookObject) =>{
+    }).then((bookObject) => {
       const allBooks = bookObject.books
       $('.list-group').append('<div id="all-books"></div>')
+      $('#all-books').html('')
+
         allBooks.forEach((bookObj) => {
          const book = `
           <img src=${bookObj.image}>
           <div>Title: ${bookObj.title}</div>
           <div>Author: ${bookObj.author}</div>
+          <div>Release Date: ${bookObj.releaseDate}</div>
+          <button id="edit" class="btn-block btn btn-primary">Edit</button>
         `
         $('#all-books').prepend(book)
+
       })
     })
+  }
+
+  const addNewBook = () => {
+    fetch('https://mutably.herokuapp.com/books', {
+      method:'post',
+      headers : {
+        'Accept' : 'application/json',
+        'Content-Type' : 'application/json'
+      },
+      body: JSON.stringify({
+        title: $('#book-title').val(),
+        author: $('#book-author').val(),
+        image: $('#book-image').val(),
+        releaseDate: $('#book-release-date').val()
+      })
+    }).then((response) => {
+      console.log(response)
+    })
+  }
+
+
+
+  // event handlers
+  $('#getAllBooks').on('click', function(e){
+    $('.error').remove()
+    $('#all-books').toggle()
+    getAllBooks()
   })
 
+  $('#addNewBook').on('click', function (e){
+    if($('#book-title').val() && $('#book-author').val() && $('#book-image').val() && $('#book-release-date').val()){
+      addNewBook()
+    } else {
+      $('.error').remove()
+      $('#addNewBook').before('<div class=error></div>')
+      $('.error').html('Please fill in all fields above')
+    }
+  })
 
+  $('.list-group').on('click', '#edit', function(){
+    if($(this).hasClass('btn-primary')){
+      $(this).html('Save')
+      $(this).removeClass('btn-primary')
+      $(this).addClass('btn-success')
+    } else {
+      $(this).html('Edit')
+      $(this).removeClass('btn-success')
+      $(this).addClass('btn-primary')
+    }
+  })
 });
